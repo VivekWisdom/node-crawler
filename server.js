499c72280e00782f23af7f4c1e66a3124a189b26
Vivek Wisdom <ervivekwisdom@gmail.com>
2017-06-08 18:02:38 +0530

    Next Level Crawl WIP

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 var app = express();
-
-app.get('/crawl', function(req, res) {
+var finalPosts = [];
+app.get('/', function(req, res, next) {
     //All the crawl code comes here.
     url = "https://futurism.com/";
-
     request(url, function(error, response, html) {
         var json = {};
+        
         if (!error) {
             var $ = cheerio.load(html);
             var postTitle,postUrl, postCategory;
             
-            var finalPosts = [];
+            
             $('.daily .container .post-details').each(function(index, elem){
                 var data = $(this);
                 json = {
@@ -35,10 +35,56 @@ app.get('/crawl', function(req, res) {
         fs.writeFile('output.json', JSON.stringify(finalPosts, null, 4), function(err) {
             console.log('File Successfully Written Check your output.json file.');
         });
-
-        res.send('Check your Console');
+        return res.redirect('/posts');
     })
-})
+});
+
+app.get('/posts', function(req, res) {
+    finalPosts.forEach(function(post,i) {
+        request(post.postUrl, function(error, response, html){
+            var allPosts = [];
+            if (!error) {
+            var $ = cheerio.load(html);
+            var title, url, category, content=[], headings=[];
+            
+            var postSingle ={
+                title:"",
+                url:"",
+                category:"",
+                content:[],
+                headings:[]
+            };
+            
+            postSingle.title = post.postTitle;
+            postSingle.url = post.postUrl;
+            postSingle.category = post.postCategory;
+            $('.post-container .post blog-content .post-content .summary p').each(function(index, elem){
+                var data = $(this);
+                
+                var para;
+                para = data.text().trim();
+                content[index] = para;
+            })
+             $('.post-container .post blog-content .post-content .summary h2').each(function(index, elem){
+                var data = $(this);
+                var heading;
+                heading = data.text().trim();
+                headings[index] = heading;
+            })
+            postSingle.content = content;
+            postSingle.headings = headings;
+
+            allPosts[i] = postSingle;
+        }
+        console.log(allPosts);
+        fs.writeFile('posts.json', JSON.stringify(allPosts, null, 4), function(err) {
+            console.log('File Successfully Written Check your posts.json file.');
+        });
+        console.log('Control is here.');
+        res.send('Check your Console.');
+        })
+    });
+});
 
 
 app.listen(8080);
